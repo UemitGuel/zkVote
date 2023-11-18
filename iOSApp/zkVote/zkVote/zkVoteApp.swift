baf75f57f241c34fb11ec9d6da241c238a121ea3
Ümit Gül <47325422+UemitGuel@users.noreply.github.com>
2023-11-18 10:19:41 +0300

    added iOS App

---
+++ iOSApp/zkVote/zkVote/zkVoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  zkVoteApp.swift
+//  zkVote
+//
+//  Created by Ümit Gül on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct zkVoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
