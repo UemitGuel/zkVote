c19104ef3898b05b1b333d94d60ac7664aa30305
Stanislaw Baranski <stan@stan.bar>
2023-11-19 03:45:43 +0300

    update migration

--- SmartContract/migrations/2_deploy_contracts_migration.js
+++ SmartContract/migrations/2_deploy_contracts_migration.js
@@ -5,13 +5,15 @@ const verifierMerkleTree = artifacts.require("verifierMerkleTree");
 const verifierZKSNARK = artifacts.require("verifierZKSNARK");
 
 
-module.exports = function(deployer, network, accounts) {
-    let usersMerkleTree = new MerkleTree(accounts.slice(1,accounts.length-1));
+module.exports = function (deployer, network, accounts) {
+    let usersMerkleTree = new MerkleTree(accounts.slice(0, accounts.length - 1).map(x => Buffer.from(x.substring(2), 'hex')));
+
+    console.log({accounts})
     deployer.deploy(Migrations);
     deployer.deploy(verifierMerkleTree);
-    deployer.deploy(verifierZKSNARK).then(function() {
-        return deployer.deploy(eVote, verifierMerkleTree.address, verifierZKSNARK.address, usersMerkleTree.getHexRoot(),300,300,300,{from:accounts[0], value:web3.utils.toWei("1","ether")})
+    deployer.deploy(verifierZKSNARK).then(function () {
+        return deployer.deploy(eVote, verifierMerkleTree.address, verifierZKSNARK.address, usersMerkleTree.getHexRoot(), 300, 300, 300, { from: accounts[0], value: web3.utils.toWei("0", "ether") })
     });
-    
-    
+
+
 };
